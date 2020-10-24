a8611ff06f51d819505c9a527698689760f4f58b
Colby McCann <colbymccann@berkeley.edu>
2020-10-24 18:42:21 +0000

    works?

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ActiveRecord::Base
   # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
   #  movies with those ratings
   # if ratings_list is nil, retrieve ALL movies
-     Movie.where(ratings_list.contains("rating"))
+     Movie.where(ratings_list.include?("rating"))
   end
   def self.all_ratings
     ['G','PG','PG-13','R']
